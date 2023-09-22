8493995c9230c6d434628692fefe8a3b4a7d9c3c
youngmo <jamong103@gmail.com>
2023-09-22 10:43:35 +0900

    풀 후 푸시

--- src/ch01/World.java
+++ src/ch01/World.java
@@ -1,9 +1,8 @@
 package ch01;
 
 public class World {
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args)
+    {
         System.out.println("World01");
         System.out.println("World02");
     }
--- src/ch02/Welcom.java
+++ src/ch02/Welcom.java
@@ -4,5 +4,6 @@ public class Welcom {
     public static void main(String[] args) {
 
         System.out.println("welcom01");
+        System.out.println("welcom02");
     }
 }
